a877f9741db90c8c58dbf90c551ec2fa93addcb3
Daniel Barclay <barclaysd@me.com>
2019-03-01 19:20:08 +0000

    Game server created to accept, decode and send data

---
+++ server.py
@@ -0,0 +1,43 @@
+import socket
+from _thread import *
+import sys
+
+server = "192.168.0.24"
+port = 5555
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+try:
+    s.bind((server, port))
+except socket.error as e:
+    str(e)
+
+s.listen(2)
+print("Waiting for connection... Server started")
+
+
+def threaded_client(conn):
+    reply = ""
+    while True:
+        try:
+            data = conn.recv(2048)
+            reply = data.decode("utf-8")
+
+            if not data:
+                print("Disconnect from server")
+                break
+            else:
+                print("Received data: {}".format(reply))
+                print("Sending data: {}".format(reply))
+
+            conn.sendall(str.encode(reply))
+        except conn.error as e:
+            print("Error: ", e)
+            break
+
+
+while True:
+    conn, addr = s.accept()
+    print("Connection made to {}".format(addr))
+
+    start_new_thread(threaded_client, conn)
