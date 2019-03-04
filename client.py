d422feb0145c28f9a293f65cd98916cc4888a78f
Daniel Barclay <daniel@urbanintel.io>
2019-03-04 23:55:21 +0000

    Refactored player class

--- client.py
+++ client.py
@@ -1,5 +1,6 @@
 import pygame
 from network import Network
+from player import Player
 # set up
 
 WIDTH = 500
@@ -8,51 +9,6 @@ HEIGHT = 500
 win = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Client")
 pygame.init()
-
-clientId = 0
-
-
-# classes
-class Player:
-    def __init__(self, x, y, width, height, color):
-        self.x = x
-        self.y = y
-        self.width = width
-        self.height = height
-        self.color = color
-        self.rect = (x, y, width, height)
-        self.val = 3
-
-    def draw(self, win):
-        pygame.draw.rect(win, self.color, self.rect)
-
-    def move(self):
-        keys = pygame.key.get_pressed()
-        if keys[pygame.K_LEFT]:
-            self.x -= self.val
-
-        if keys[pygame.K_RIGHT]:
-            self.x += self.val
-
-        if keys[pygame.K_UP]:
-            self.y -= self.val
-
-        if keys[pygame.K_DOWN]:
-            self.y += self.val
-
-        self.update()
-
-    def update(self):
-        self.rect = (self.x, self.y, self.width, self.height)
-
-
-def read_position(string):
-    string = string.split(",")
-    return int(string[0]), int(string[1])
-
-
-def make_position(tup):
-    return str(tup[0]) + "," + str(tup[1])
 
 
 # functions
