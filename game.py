db5e06fe6b44145945f04343ae59b54f50937e0c
Daniel Barclay <barclaysd@me.com>
2019-03-24 00:14:47 +0000

    Added game logic

--- game.py
+++ game.py
@@ -9,5 +9,45 @@ class Game:
         self.draws = 0
 
     def get_player_move(self, p):
-        """""""
+        """"
+        :param p [0, 1]
+        :return Move
+        """
         return self.moves[p]
+
+    def player(self, player, move):
+        self.moves[player] = move
+        if player == 0:
+            self.p1_went = True
+        else:
+            self.p2_went = True
+
+    def connected(self):
+        return self.ready
+
+    def both_went(self):
+        return self.p1_went and self.p2_went
+
+    def winner(self):
+        p1 = self.moves[0].upper()[0]
+        p2 = self.moves[1].upper()[0]
+
+        winner = -1
+        if p1 == "R" and p2 == "S":
+            winner = 0
+        elif p1 == "S" and p2 == "R":
+            winner = 1
+        elif p1 == "S" and p2 == "P":
+            winner = 0
+        elif p1 == "R" and p2 == "P":
+            winner = 1
+        elif p1 == "P" and p2 == "S":
+            winner = 1
+        elif p1 == "P" and p2 == "R":
+            winner = 0
+
+        return winner
+
+    def reset_went(self):
+        self.p1_went = False
+        self.p2_went = False
